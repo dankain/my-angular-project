7950881e4b95bf540718582deff27545c2f8dbed
Dan Kain <dan@cabiri.io>
2023-11-14 10:39:12 +0000

    with middleware

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,29 @@
 import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
+import {HttpHeaders} from "@angular/common/http";
 
 const uri = 'https://spacex-production.up.railway.app/'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+
+  const http = httpLink.create({ uri(operation) {
+      return `${uri}?o=${operation.operationName}`
+    } });
+  const middleware = new ApolloLink((operation, forward) => {
+    operation.setContext({
+      headers: new HttpHeaders().set(
+        'Authorization',
+        'Bearer dan-test',
+      ),
+    });
+    return forward(operation);
+  });
+
+  const link = middleware.concat(http);
+
   return {
-    link: httpLink.create({ uri }),
+    link,
     cache: new InMemoryCache(),
   };
 }
