3b90a8a1914c02a3d5c77e7c050b5faa86511a77
Dan Kain <dan@cabiri.io>
2023-11-14 10:22:45 +0000

    working space launches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,40 +2,25 @@ import { Apollo, gql } from 'apollo-angular';
 import { Component, OnInit } from '@angular/core';
 
 
-const SEARCH_PRODUCTS = gql`
-  query searchProducts($text: String!, $locale: String!) {
-    searchProducts(text: $text, locale: $locale) {
-      edges {
-        node {
-          id
-          title
-          slug
-        }
-      }
-    }
-  }
-`
-
 @Component({
   selector: 'app',
   template: `
     <h1>Launches</h1>
     <div *ngIf="loading">Loading...</div>
     <div *ngIf="error">Error :(</div>
-    <div *ngIf="searchProducts">
+    <div *ngIf="launches">
 
-      <div *ngFor="let node of searchProducts.edges">
-        <p><b>{{ node.title }}</b></p>
-        <p>{{ node.slug }}</p>
+      <div *ngFor="let launch of launches">
+        <p><b>{{ launch.mission_name }}</b></p>
+        <p>{{ launch.details }}</p>
       </div>
     </div>
   `,
 })
 
-
 export class AppComponent implements OnInit{
   title = 'my-angular-project';
-  searchProducts: any;
+  launches: any[] = []
   loading = true;
   error: any;
 
@@ -44,13 +29,19 @@ export class AppComponent implements OnInit{
   ngOnInit() {
     this.apollo
     .watchQuery({
-      query: SEARCH_PRODUCTS,
-      variables: {text: "", locale: "en-gb"}
-
+      query: gql`query launches
+        {
+          launches {
+            launch_date_local
+            id
+            details
+            mission_name
+          }
+        }
+      `,
     })
     .valueChanges.subscribe((result: any) => {
-      console.log(result);
-      this.searchProducts = result.data?.searchProducts;
+      this.launches = result.data?.launches;
       this.loading = result.loading;
       this.error = result.error;
     });
