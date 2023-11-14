0fa92c6a1c904406630faa8654059fa63b56a7b9
Dan Kain <dan@cabiri.io>
2023-11-14 12:10:02 +0000

    persistent queries

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,24 +3,30 @@ import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
 import {HttpHeaders} from "@angular/common/http";
+import { createPersistedQueryLink } from 'apollo-angular/persisted-queries';
+import { sha256 } from 'crypto-hash';
 
 const uri = 'https://spacex-production.up.railway.app/'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
 
-  const http = httpLink.create({ uri(operation) {
-      return `${uri}?o=${operation.operationName}`
-    } });
+
   const middleware = new ApolloLink((operation, forward) => {
     operation.setContext({
-      headers: new HttpHeaders().set(
-        'Authorization',
-        'Bearer dan-test',
+      headers: new HttpHeaders({ 'Authorization': 'Bearer dan-test', 'Content-type': 'application/json'}
       ),
     });
     return forward(operation);
   });
 
-  const link = middleware.concat(http);
+  const link = middleware.concat(createPersistedQueryLink({ useGETForHashedQueries: true, sha256 }).concat(
+    httpLink.create({ uri(operation) {
+        return `${uri}?o=${operation.operationName}`
+      } })
+  ));
+  //
+  // const linkChain = createPersistedQueryLink({ useGETForHashedQueries: true, sha256 }).concat(
+  //   httpLink.create({ uri }),
+  // );
 
   return {
     link,
